a4bb250797403d5e6a7cf80495fe7cf6292f4295
Amin Rahkan <Amin.rahkan7@gmail.com>
2017-08-02 12:19:59 +0430

    MakeCallActivity

---
+++ app/src/main/java/aminrahkan7/sipapp/MakeCallActivity.java
@@ -0,0 +1,19 @@
+package aminrahkan7.sipapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MakeCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_call);
+
+    }
+
+}
